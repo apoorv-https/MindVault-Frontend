17ac35b3d183f3e1d9eaec4c690f83b4fa257472
Apoorv <k.apoorvtiwari@gmail.com>
2025-10-26 18:01:07 +0530

    Adding chnges

--- src/config.ts
+++ src/config.ts
@@ -1,2 +1,3 @@
 // config.ts
-export const BACKEND_URL = import.meta.env.VITE_BACKEND_URL || "http://localhost:3000";
+export const BACKEND_URL ="https://mind-vault-backend-rosy.vercel.app"
+//  import.meta.env.VITE_BACKEND_URL || "http://localhost:3000";
